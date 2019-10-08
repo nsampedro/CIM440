2ec33ac7188cbfed2d827641143ad3c5ecec97b1
Nicolas Sampedro <nxs310@miami.edu>
2019-10-07 20:50:20 -0400

    seasons
    finishing the seasons example

--- Class examples/seasons/sketch.js
+++ Class examples/seasons/sketch.js
@@ -1,6 +1,9 @@
 var seasonSelect;
 var seasonType = "";
 
+var sunX = 0;
+var sunY = 0;
+
 
 function setup() {
   // put setup code here
@@ -10,8 +13,43 @@ function setup() {
   seasonSelect.option("");
   seasonSelect.option("fall");
   seasonSelect.option("winter");
+  seasonSelect.option("spring");
+  seasonSelect.option("summer");
+  seasonSelect.changed(function(){
+      seasonType = seasonSelect.value();
+      //.value holds fall, winter, summer; passing that value to seasonType
+  });
+
+  sunX = width/2; //do this in the setup because you need the width and height to exist; dont exist outside of setup/drawing
+  sunY = height*2;
 }
 
 function draw() {
   // put drawing code here
+  background(255);
+  if(seasonType == "fall"){
+      console.log("fall");
+  }else if(seasonType == "winter"){
+      console.log("winter")
+  }else if(seasonType == "spring"){
+      console.log("spring");
+  }else if(seasonType == "summer"){
+      console.log("summer");
+      fill("orange");
+      stroke("red");
+      ellipse(sunX,sunY,width,height);
+
+      if(sunY > height){
+          sunY = sunY -1;// allows us to move the sun up the screen when "summer is selected"
+      }
+
+  }else{
+    console.log("blank");
+    text("Make a selection",100,20);  
+  }
+
+
+
+
+
 }
