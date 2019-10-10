2865a39fcebd9814f0d25684a4e599155c88bec9
Nicolas Sampedro <nxs310@miami.edu>
2019-10-10 08:19:01 -0400

    class
    working on falling leaves/snow examples

--- Class examples/seasons/sketch.js
+++ Class examples/seasons/sketch.js
@@ -4,6 +4,21 @@ var seasonType = "";
 var sunX = 0;
 var sunY = 0;
 
+var snowX =[];
+var snowY =[];
+var snowAmmount = 100;
+
+
+var leafX =[];
+var leafY =[];
+var leafAmmount = 100;
+var leafImage;
+
+function preload(){
+    leafImage = loadImage("leaf.png");
+
+}
+
 
 function setup() {
   // put setup code here
@@ -22,6 +37,32 @@ function setup() {
 
   sunX = width/2; //do this in the setup because you need the width and height to exist; dont exist outside of setup/drawing
   sunY = height*2;
+
+
+//var i=0 is our start point
+//i<100 conditional, how many times we want the for loop to run
+//i++, which is the same as saying i=i+1
+for(var i = 0; i<snowAmmount; i++){
+    snowX[i] = random(0,width); //random number is from 0-width-1 or 0 to 399
+    snowY[i] = random(0, -500);
+
+}
+
+//var i is destroyed after loop is done
+
+
+console.log("snowX" + snowX);
+console.log("snowY" + snowY);
+
+for(var i = 0; i< leafAmmount; i++){
+    leafX[i] = random(0,width);
+    leafY[i] = random(0,-500);
+}
+
+console.log("leafX" + leafX);
+console.log("leafY" + leafY);
+
+
 }
 
 function draw() {
@@ -29,8 +70,38 @@ function draw() {
   background(255);
   if(seasonType == "fall"){
       console.log("fall");
+
+      for(var f = 0; f<leafAmmount; f++){
+          if(leafY[f]<height-20){
+              leafY[f]++;
+              leafX[f] = leafX[f] + sin(radians(frameCount));
+              //console.log("leafY " + f + " " + leafY[f]);
+          }
+
+          image(leafImage,leafX[f], leafY[f], 20,30);
+
+      }
+
+      //console.log("leafY " + leafY);
+
   }else if(seasonType == "winter"){
-      console.log("winter")
+      console.log("winter");
+      fill(255);
+      stroke(0);
+
+      for(var y = 0; y<snowAmmount; y++){
+          snowY[y]++; // same as snowY[y] = snowY[y] +1;
+          ellipse(snowX[y], snowY[y],10,10);
+
+          if(snowY[y] > height){
+            snowY[y]= random(0,-500);
+            snowX[y] = random(0,width);
+
+          }
+      }
+
+
+
   }else if(seasonType == "spring"){
       console.log("spring");
   }else if(seasonType == "summer"){
