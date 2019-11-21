69a89059e076354e67256ffc5f92ef536f360ee4
Nicolas Sampedro <nxs310@miami.edu>
2019-11-21 11:00:00 -0500

    working on final
    adding animals

--- Homework/Final/sketch.js
+++ Homework/Final/sketch.js
@@ -16,6 +16,15 @@ const options = {
 var nigeria;
 var china;
 var borneo;
+var india;
+var florida;
+var carolina;
+var california;
+var brasil;
+var zimbabwe;
+var spain;
+var australia;
+var arctic;
 
 
 // p5.js setup
@@ -58,14 +67,40 @@ function drawPoint() {
  clear();
 
  nigeria = myMap.latLngToPixel(11.396396, 5.076543);
- ellipse(nigeria.x, nigeria.y, 20, 20);
+ ellipse(nigeria.x, nigeria.y, 20, 20); //Western lowland gorilla
 
  china = myMap.latLngToPixel(44.891512, 134.866455);
- ellipse(china.x, china.y, 20, 20);
+ ellipse(china.x, china.y, 20, 20); //Amur Leopard
 
  borneo = myMap.latLngToPixel(0.147800, 114.272526);
- ellipse(borneo.x, borneo.y, 20, 20);
+ ellipse(borneo.x, borneo.y, 20, 20); //bornean orangutan
 
+ india = myMap.latLngToPixel(10.796548, 78.345543);
+ ellipse(india.x, india.y, 20, 20); //asian elephant
+
+ florida = myMap.latLngToPixel(26.002728, -81.112147);
+ ellipse(florida.x, florida.y, 20, 20); // florida panther
+
+ carolina = myMap.latLngToPixel(35.742507, -82.373001);
+ ellipse(carolina.x, carolina.y, 20, 20); // red wolf
+
+ california = myMap.latLngToPixel(32.638941, -114.684248);
+ ellipse(california.x, california.y, 20, 20); //california condor
+
+ brasil = myMap.latLngToPixel(-4.088932, -63.647648);
+ ellipse(brasil.x, brasil.y, 20, 20); //spix macaw
+
+ zimbabwe = myMap.latLngToPixel(-18.425344, 30.397657);
+ ellipse(zimbabwe.x, zimbabwe.y, 20, 20); //black rhino
+
+ spain = myMap.latLngToPixel(37.593016, -7.032537);
+ ellipse(spain.x, spain.y, 20, 20); //Iberian lynx
+
+ australia = myMap.latLngToPixel(-20.518358, 142.375377);
+ ellipse(australia.x, australia.y, 20, 20); //Southern Cassowary
+
+ arctic = myMap.latLngToPixel(79.781164, 1.457323);
+ ellipse(arctic.x, arctic.y, 20, 20); //narwhal
 
 
 }
